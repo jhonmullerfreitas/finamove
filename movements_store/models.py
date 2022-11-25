d2774730a7cae6eaa1718c1d951ebe7ea90e78eb
Jhon Muller <jmuller.jhon80@gmail.com>
2022-11-25 16:50:42 -0400

    feat: normalização de retorno da response, padronização de id.

--- movements_store/models.py
+++ movements_store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class TransactionType(models.TextChoices):
     DEBITO = "Débito"
@@ -14,17 +15,29 @@ class TransactionType(models.TextChoices):
 
 class MovementStore(models.Model):
 
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+        editable=False
+    )
+    
     transaction_type = models.CharField(
         max_length=100,
         choices=TransactionType.choices,
         default=TransactionType.DEBITO
     )
-    date_of_currence = models.CharField(max_length=8)
+    date_of_currence = models.CharField(max_length=11)
     transaction_value = models.DecimalField(max_digits=10, decimal_places=2)
     card = models.CharField(max_length=12)
     time = models.CharField(max_length=8)
-    store_name = models.ForeignKey(
+    store = models.ForeignKey(
         "stores.Store",
         on_delete=models.CASCADE,
         related_name="movements_store"
+    )
+
+    doc_cnab = models.ForeignKey(
+        'movements.Movement',
+        on_delete=models.CASCADE,
+        related_name='movement_list'
     )
